3a51a856ca16e68145af873affc3013239008431
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-11-04 10:06:14 -0500

    n

--- src/main/java/com/Lookup/UserMicroservice/User/UserControllerImplementation.java
+++ src/main/java/com/Lookup/UserMicroservice/User/UserControllerImplementation.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/user")
 @RestController
 public class UserControllerImplementation implements UserController {
     private final UserService userService;
